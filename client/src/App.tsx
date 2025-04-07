74f9d5d3c87896bddd95434e87495fa79da56c82
brandon park <brandonpark206@gmail.com>
2025-04-06 23:21:15 -0700

    adding logic for refresh key

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, CssBaseline, Box } from '@mui/material';
 import CsvUploader from './components/CsvUploader';
 import DataTable from './components/DataTable';
 
 const App: React.FC = () => {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleUploadSuccess = () => {
+    setRefreshKey(prev => prev + 1);  // Trigger re-render
+  };
+
   return (
     <>
       <CssBaseline />
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
-          <CsvUploader />
+          <CsvUploader onUploadSuccess={handleUploadSuccess} />
         </Box>
-        <DataTable />
+        <DataTable key={refreshKey} />
       </Container>
     </>
   );
